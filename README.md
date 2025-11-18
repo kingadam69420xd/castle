4fc324278961087b5af78c0cf0048dfbaf661ee6
adam <nodev1@proton.me>
2025-11-18 08:25:00 +0000

    foo bar baz quz quux

---
+++ README.md
@@ -0,0 +1,2 @@
+# CASTLE
+A single mono-repo of reference code and algorithms. Don't know if it is a good idea to do it like this.
---
+++ src/main.zig
@@ -0,0 +1,7 @@
+const std = @import("std");
+const castle = @import("castle");
+const print = std.debug.print;
+
+pub fn main() void {
+    
+}
